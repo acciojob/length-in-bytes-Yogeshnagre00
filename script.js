206bc9afc4d50f799564b0c8c2d386882d08f83b
Yogesh Nagre <119948989+Yogeshnagre00@users.noreply.github.com>
2023-06-02 09:42:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const byteSize = (str) => {
-  // write your code here
+  // write your code her
+	const encoder = new TextEncoder();
+  const encodedStr = encoder.encode(str);
+  
+  // Return the length of the encoded string in bytes
+  return encodedStr.length;
+	
 };
+
 
 // Do not change the code below
 const str = prompt("Enter some string.");
